7665e3c94310d1f2f6aaf39ba34fe237c37b20c2
juhwanlee <juhwanlee@diquest.com>
2024-02-19 01:04:30 +0000

    index list IQR

--- Structured/outlier_IQR.py
+++ Structured/outlier_IQR.py
@@ -30,7 +30,7 @@ def remove_outliers_combined_IQR(df, columns):
         indices_to_keep.intersection_update(valid_indices)  # 유효한 인덱스만 유지
 
     # 이상치가 아닌 행만 포함하는 데이터프레임 반환
-    filtered_df = df.loc[indices_to_keep]
+    filtered_df = df.loc[list(indices_to_keep)]
     return filtered_df
 
 
